32583792d75fbea6c1f0e815c93c439889501c58
ars707 <47089419+ars707@users.noreply.github.com>
2019-02-14 21:35:25 +0400

    Add files via upload

---
+++ lesson5/home/tip1/js/script.js
@@ -0,0 +1,8 @@
+let menu = document.getElementsByClassName('menu');
+// let tox = document.querySelectorAll('.menu-item');
+
+
+let pyat = document.createElement('li');
+let pyat = innerText('pyatiy elenet')
+menu.appendChild(pyat);
+
